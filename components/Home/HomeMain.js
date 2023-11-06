f5be2a438e5caa9c270937aaaa5ea48bfc8fa13a
danm9 <daniel.minahan9@gmail.com>
2023-11-05 22:19:35 -0500

    home page and navbar to mobile

--- components/Home/HomeMain.js
+++ components/Home/HomeMain.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import useDeviceDetect from "../../utils/useDeviceDetect";
 import { Container } from "react-bootstrap";
 import Home1 from "../../public/Images/Home1.png";
 import styles from "../../styles/Home.module.css";
 
 export function HomeMain() {
+  const { isMobile } = useDeviceDetect();
+
   const homeMainData = [
     {
       titleContent: "Sell Your House Fast in the CSRA",
@@ -21,31 +24,60 @@ export function HomeMain() {
 
   return (
     <>
-      <div className="d-flex flex-column p-5">
-        <p className="fs-2 fw-bold text-center">
-          {homeMainData[0].titleContent}
-        </p>
-        <div className="d-flex align-items-center">
-          <Container className="d-flex justify-content-around">
-            <Image src={Home1} className={styles.image} />
+      {isMobile ? (
+        <div className="d-flex flex-column pt-5">
+          <p className="fs-2 fw-bold text-center">
+            {homeMainData[0].titleContent}
+          </p>
+          <Container className="d-flex justify-content-center">
+            <Image src={Home1} className={styles.mobileImage} />
           </Container>
-          <Container className="d-flex flex-column">
-            <p className="w-75 fs-4">{homeMainData[0].textContent}</p>
-            <p className="fs-5">
-              <strong className="text-danger">X</strong>{" "}
-              {homeMainData[0].noContent.first}
-            </p>
-            <p className="fs-5">
-              <strong className="text-danger">X</strong>{" "}
-              {homeMainData[0].noContent.second}
-            </p>
-            <p className="fs-5">
-              <strong className="text-danger">X</strong>{" "}
-              {homeMainData[0].noContent.third}
-            </p>
+          <Container className="d-flex flex-column justify-content-center p-3">
+            <p className="fs-4">{homeMainData[0].textContent}</p>
+            <Container>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.first}
+              </p>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.second}
+              </p>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.third}
+              </p>
+            </Container>
           </Container>
         </div>
-      </div>
+      ) : (
+        // Browser
+        <div className="d-flex flex-column p-5">
+          <p className="fs-2 fw-bold text-center">
+            {homeMainData[0].titleContent}
+          </p>
+          <div className="d-flex align-items-center">
+            <Container className="d-flex justify-content-around">
+              <Image src={Home1} className={styles.image} />
+            </Container>
+            <Container className="d-flex flex-column">
+              <p className="w-75 fs-4">{homeMainData[0].textContent}</p>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.first}
+              </p>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.second}
+              </p>
+              <p className="fs-5">
+                <strong className="text-danger">X</strong>{" "}
+                {homeMainData[0].noContent.third}
+              </p>
+            </Container>
+          </div>
+        </div>
+      )}
     </>
   );
 }
