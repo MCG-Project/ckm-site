d4fb3c6cc85c3ab5ce8353009f555019428c7204
danm9 <daniel.minahan9@gmail.com>
2023-11-15 13:30:02 -0500

    updated with useeffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactGA from "react-ga";
 import { Analytics } from "@vercel/analytics/react";
 import { NavBar } from "../components/NavBar/NavBar";
 import { Footer } from "../components/Footer/Footer";
 import styles from "../styles/_app.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
